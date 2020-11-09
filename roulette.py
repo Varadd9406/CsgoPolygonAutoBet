a59c2c1c92ec6862aa00277ffe259909662e6ca3
Varad Deouskar <52121257+Varadd9406@users.noreply.github.com>
2020-11-09 15:36:54 +0530

    added refresh

--- roulette.py
+++ roulette.py
@@ -13,7 +13,6 @@ driver.get("https://www.csgopolygon.com")
 signin = driver.find_element_by_class_name("window_steam_button")
 signin.click()
 time.sleep(3)
-
 driver.find_element_by_id("steamAccountName").send_keys(STEAM_USER)
 driver.find_element_by_id("steamPassword").send_keys(STEAM_PASS)
 driver.find_element_by_id("imageLogin").click()
@@ -25,20 +24,30 @@ else:
 game = Roulette(driver)
 finalBase = 3
 base = finalBase
+cnt = 0
 while True:
+	if cnt >= 5:
+		driver.refresh()
+		cnt = 0
 	initial = game.findBalance()
 	game.placeBet(base)
 	time.sleep(1)
 	if game.bettedAmount() == 0:
+		cnt += 1
 		time.sleep(8)
+		cnt += 1
 		continue
 	time.sleep(34)
 	final = game.findBalance()
 	if final == initial:
+		cnt += 1
 		continue
 	elif final > initial:
+		cnt = 0
 		print('Won')
 		base = finalBase
 	elif final < initial:
+		cnt = 0
 		base *= 2
 		print('Trust the algo:', base)
+	cnt += 1
